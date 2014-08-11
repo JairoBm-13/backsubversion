420880a596ec2ad4c956cc93bb5a4a843f553ba2
JairoBm-13 <j.bautista.m13@outlook.com>
2014-08-11 14:30:57 -0500

    Primer commit del proyecto a ver si sube

---
+++ svnBasico/src/main/webapp/src/controller/userController.js
@@ -0,0 +1,37 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+App.Controller.UserController = Backbone.View.extend({
+        el: '#main',
+        initialize: function(options) {
+            this.editTemplate = _.template($('#user').html());
+            var self = this;
+            Backbone.on('user-create', function(params) {
+                self.create(params);
+            });
+            Backbone.on('user-save', function(params) {
+                self.save(params);
+            });
+            Backbone.on('user-cancel', function(params) {
+                self.cancel(params);
+            });
+        },
+        create: function() {
+            this.userModel = new App.Model.UserModel();
+            this._renderEdit();
+        },
+        save:function() { 
+            var model = $('#userForm').serializeObject();
+            document.getElementById('msj').innerHTML =  "<div class=\"alert alert-success\"> <a href=\"#\" class=\"close\" data-dismiss=\"alert\">&times</a>El usuario "+ model.firstName + " " +model.lastName+ " nació el "+ model.birthDate+"</div>";
+        },
+        cancel: function(){
+            alert('Cancel');
+        },
+        _renderEdit: function() {
+            var self = this;
+            self.$el.html(self.editTemplate({user: self.userModel}));
+        }
+    });
+
